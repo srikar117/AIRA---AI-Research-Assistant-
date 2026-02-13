caf17af18b88d29e9d8c8c36a4b9d03c2b598555
srikar117 <maddukurisrikar@gmail.com>
2026-02-13 21:18:46 +0530

    corrected api key

--- llm_engine.py
+++ llm_engine.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 import os
 
-api_key = os.getenv("GEMNIN_API_KEY")
+api_key = os.getenv("GEMINI_API_KEY")
 
 def initialize_llm(api_key):
     genai.configure(api_key = api_key)
