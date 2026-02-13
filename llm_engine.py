54812ef202776eaec927c604aaba596e1c5c357a
srikar117 <maddukurisrikar@gmail.com>
2026-02-13 21:16:09 +0530

    initialised gemini as the llm

---
+++ llm_engine.py
@@ -0,0 +1,9 @@
+import google.generativeai as genai
+import os
+
+api_key = os.getenv("GEMNIN_API_KEY")
+
+def initialize_llm(api_key):
+    genai.configure(api_key = api_key)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    return model
